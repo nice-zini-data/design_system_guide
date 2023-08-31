3b2698a2833605c3c8137e744c6c44a726617db3
gslee <kleeeec@gmail.com>
2023-08-31 09:35:52 +0900

    2023-08-31 -- 내용 수정

---
+++ src/main/java/com/zinidata/eatout/vo/output/AgileMenuOutVO.java
@@ -0,0 +1,17 @@
+package com.zinidata.eatout.vo.output;
+
+import lombok.Data;
+
+@Data
+public class AgileMenuOutVO {
+
+    //output
+    private String menuCd;
+    private String menuNm;
+    private String menu1Cd;
+    private String menu1Nm;
+    private String menu2Cd;
+    private String menu2Nm;
+    private String menu3Cd;
+    private String menu3Nm;
+}
